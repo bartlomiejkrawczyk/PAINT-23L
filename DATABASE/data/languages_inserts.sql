4071fa1baa969b561762ac4ba60c0a8d4a831a80
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-06-04 10:58:04 +0200

    Update word inserts

--- DATABASE/data/languages_inserts.sql
+++ DATABASE/data/languages_inserts.sql
@@ -1,2 +1,3 @@
 INSERT INTO languages(language_id, code, name) VALUES (1, 'ENG', 'English');
 INSERT INTO languages(language_id, code, name) VALUES (2, 'PLN', 'Polski');
+INSERT INTO languages(language_id, code, name) VALUES (3, 'ITL', 'Italiano');
