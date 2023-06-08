d548f18fd95c124b30acc56a010d4b14b5eb0817
Jakub Marcowski <01158831@pw.edu.pl>
2023-06-08 03:21:45 +0200

    Update database scripts

--- DATABASE/data/data_inserts.py
+++ DATABASE/data/data_inserts.py
@@ -1,23 +1,27 @@
 from os import listdir, remove, path
 
 LANGUAGES = {
-    'en': 1,
-    'pl': 2,
-    'it': 3,
+    "wordle": 0,
+    "en": 1,
+    "pl": 2,
+    "it": 3,
 }
 
 
 def list_files(language: str) -> list[str]:
-    return listdir(f'{language}/')
+    return listdir(f"{language}/")
+
 
 def read_words(directory: str, file: str) -> list[str]:
-    with open(f'{directory}/{file}') as file_handle:
+    with open(f"{directory}/{file}") as file_handle:
         return [line.strip() for line in file_handle]
+
 
 def format_word(word_id: int, language_id: int, word_num: int, word: str) -> str:
     return f"INSERT INTO words(word_id, language_id, word_length, word_number, word) VALUES ({word_id}, {language_id}, {len(word)}, {word_num}, '{word}');\n"
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     if path.exists("words_inserts.sql"):
         remove("words_inserts.sql")
     i = 0
@@ -25,7 +29,6 @@ if __name__=="__main__":
         for file in list_files(key):
             for word_num, word in enumerate(read_words(key, file)):
                 sql = format_word(i, id, word_num, word)
-                with open("words_inserts.sql", "a") as word_inserts:
+                with open("words_inserts.sql", 'a') as word_inserts:
                     word_inserts.write(sql)
                 i += 1
-
--- DATABASE/data/languages_inserts.sql
+++ DATABASE/data/languages_inserts.sql
@@ -1,3 +1,4 @@
+INSERT INTO languages(language_id, code, name) VALUES (0, 'WRD', 'Wordle');
 INSERT INTO languages(language_id, code, name) VALUES (1, 'ENG', 'English');
 INSERT INTO languages(language_id, code, name) VALUES (2, 'PLN', 'Polski');
 INSERT INTO languages(language_id, code, name) VALUES (3, 'ITL', 'Italiano');
